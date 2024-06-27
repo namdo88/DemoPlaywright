0007e1fde498d132986c0e70a19b2886e0675fcd
namdo88 <nam.doh21@outlook.com>
2024-06-27 13:49:55 +0700

    Update autoPlaywrightDemo.spect.ts

--- tests/autoPlaywrightDemo.spect.ts
+++ tests/autoPlaywrightDemo.spect.ts
@@ -2,17 +2,13 @@ import { test, expect } from '@playwright/test';
 import { auto } from "auto-playwright";
 
 test("executes query, action and assertion", async ({page}) => {
-    const options = {
-        model: "gpt-3.5-turbo",
-        openaiApiKey: 'sk-proj-...'
-    };
 
     // Navigate to a website
     await page.goto("https://www.saucedemo.com/v1/");
   
     // `auto` can query data
     // In this case, the result is plain-text contents of the header
-    const res = await auto("get the header text", { page, test }, options);  
+    const res = await auto("get the header text", { page, test });  
 
 
     // use res.query to get a query result.
